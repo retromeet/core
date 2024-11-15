9efab565949ef9e9b01d6aaa96f3d90b92d71f11
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-15 17:30:49 +0100

    Fix location repository function names

--- app/persistence/repository/location.rb
+++ app/persistence/repository/location.rb
@@ -12,18 +12,18 @@ module Persistence
           # TODO: (renatolond, 2024-11-14) It seems .returning(:id) is only supported for merge on pg >=17
           # For now doing two operations, but fix to do only one when possible
 
-          account_informations.merge_using(Database.connection.select(Sequel.as(location_result.osm_id, :osm_id)).as(:u), Sequel[:locations][:osm_id] => Sequel[:u][:osm_id])
-                              .merge_insert(osm_id: location_result.osm_id, display_name: display_name_json, country_code: location_result.country_code, geom:)
-                              .merge_update(Sequel.pg_jsonb_op(:display_name)[location_result.language] => Sequel.pg_jsonb_wrap(location_result.display_name), country_code: location_result.country_code, geom:)
-                              .merge
+          locations.merge_using(Database.connection.select(Sequel.as(location_result.osm_id, :osm_id)).as(:u), Sequel[:locations][:osm_id] => Sequel[:u][:osm_id])
+                   .merge_insert(osm_id: location_result.osm_id, display_name: display_name_json, country_code: location_result.country_code, geom:)
+                   .merge_update(Sequel.pg_jsonb_op(:display_name)[location_result.language] => Sequel.pg_jsonb_wrap(location_result.display_name), country_code: location_result.country_code, geom:)
+                   .merge
 
-          account_informations.where(osm_id: location_result.osm_id).get(:id)
+          locations.where(osm_id: location_result.osm_id).get(:id)
         end
 
         private
 
           # @return [Sequel::Postgres::Dataset]
-          def account_informations
+          def locations
             Database.connection[:locations]
           end
       end
