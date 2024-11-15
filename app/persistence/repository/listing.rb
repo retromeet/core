86116c0c43f93b1e8b5aef48ab85d47e947eb14e
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-15 18:39:24 +0100

    WIP: create test for listing

--- app/persistence/repository/listing.rb
+++ app/persistence/repository/listing.rb
@@ -5,18 +5,20 @@ module Persistence
     # Contains database logic around the account listing
     module Listing
       class << self
-        def nearby(account_id:, distance_in_meters: 5_000)
+        # Returns accounts that are near the requested Account
+        # will paginate results with min_account_id
+        def nearby(account_id:, min_account_id: nil, distance_in_meters: 5_000)
           profile_location = Persistence::Repository::Account.profile_location(account_id:)
 
           location_subquery = locations.select(Sequel.lit("geom::geography <-> ?::geography", profile_location).as(:dist))
                                        .select_append(:id, Sequel[:display_name].as(:location_display_name))
                                        .where(Sequel.function(:ST_DWithin, Sequel.lit("geom::geography"), Sequel.lit("?::geography", profile_location), distance_in_meters))
                                        .order(:dist)
-          pp location_subquery.to_a
-          account_informations.inner_join(location_subquery, id: :location_id)
-                              .select(:display_name, :account_id, :birth_date, :genders, :orientations, :relationship_status, :location_display_name)
-                              .exclude(Sequel[:account_informations][:account_id] => account_id)
-                              .to_a
+          accounts = account_informations.inner_join(location_subquery, id: :location_id)
+                                         .select(:display_name, :account_id, :birth_date, :genders, :orientations, :relationship_status, :location_display_name)
+                                         .exclude(Sequel[:account_informations][:account_id] => account_id)
+          accounts.where { account_id > min_account_id } if min_account_id
+          accounts.to_a
         end
 
         private
--- test/app/api/authenticated/listing_test.rb
+++ test/app/api/authenticated/listing_test.rb
@@ -5,9 +5,20 @@ require_relative "../../../test_helper"
 describe API::Authenticated::Profile do
   include RackHelper
   before(:all) do
+    # Create a few locations to create accounts around
+    @schaerbeek = create(:location, latitude: 50.8676041, longitude: 4.3737121, language: "en", name: "Schaerbeek - Schaarbeek, Brussels-Capital, Belgium", country_code: "be", osm_id: 58_260)
+    @etterbeek = create(:location, latitude: 50.8361447, longitude: 4.3861737, language: "en", name: "Etterbeek, 1040 Brussels-Capital, Belgium", country_code: "be", osm_id: 58_252)
+    @amsterdam = create(:location, latitude: 52.3730796, longitude: 4.8924534, language: "en", name: "Amsterdam, North Holland, Netherlands", country_code: "nl", osm_id: 271_110)
+    @cologne = create(:location, latitude: 50.938361, longitude: 6.959974, language: "en", name: "Cologne, North Rhine-Westphalia, Germany", country_code: "de", osm_id: 62_578)
+    @paris = create(:location, latitude: 48.8534951, longitude: 2.3483915, language: "en", name: "Paris, France", country_code: "fr", osm_id: 71_525)
+
     @login = "foo@retromeet.social"
     @password = "bogus123"
-    @account = create(:account, email: @login, password: @password, account_information: { display_name: "Foo", created_at: Time.new(2024, 9, 20, 16, 50, 0) })
+    @account = create(:account, email: @login, password: @password, account_information: { display_name: "Foo", created_at: Time.new(2024, 9, 20, 16, 50, 0), location_id: @schaerbeek.id })
+    @etterbeek_account = create(:account, account_information: { display_name: "Etterbeek account", location_id: @etterbeek.id })
+    @amsterdam_account = create(:account, account_information: { display_name: "Amsterdam account", location_id: @amsterdam.id })
+    @cologne_account = create(:account, account_information: { display_name: "Cologne account", location_id: @cologne.id })
+    @paris_account = create(:account, account_information: { display_name: "Paris account", location_id: @paris.id })
   end
 
   describe "get /listing" do
@@ -16,9 +27,9 @@ describe API::Authenticated::Profile do
       @endpoint = "/api/listing"
     end
 
-    it "Gets a list of the nearest profiles in order" do
-      expected_response = { display_name: "Foo", created_at: Time.new(2024, 9, 20, 16, 50, 0) }
-      authorized_get @auth, "/api/profile/info"
+    it "With the default distance, gets only the account in Etterbeek" do
+      expected_response = { profiles: [] }
+      authorized_get @auth, @endpoint
 
       assert_predicate last_response, :ok?
       assert_equal expected_response, JSON.parse(last_response.body, symbolize_names: true)
