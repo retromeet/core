3ed9ce75dea1ec43b25fdbdc822879fc7aa9aade
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-11-19 16:18:02 +0100

    Add uuid validation to api

--- app/api/authenticated/profile.rb
+++ app/api/authenticated/profile.rb
@@ -84,7 +84,7 @@ module API
         end
         namespace ":id" do
           params do
-            requires :id, type: String
+            requires :id, type: String, regexp: /\A[0-9A-F]{8}-[0-9A-F]{4}-7[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}\z/i
           end
 
           desc "Returns the complete profile information for the requested account id.",
