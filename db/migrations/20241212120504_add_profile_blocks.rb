d34e9f75b8e57106eb5dd28063291e824fdd7912
Renato "Lond" Cerqueira <renato@lond.com.br>
2024-12-18 12:46:46 +0100

    Create profile blocks table

---
+++ db/migrations/20241212120504_add_profile_blocks.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table(:profile_blocks) do
+      primary_key :id
+      column :created_at, :timestamptz, null: false, default: Sequel::CURRENT_TIMESTAMP
+      foreign_key :profile_id, :profiles, null: false, type: :uuid
+      foreign_key :target_profile_id, :profiles, null: false, type: :uuid
+    end
+  end
+end
