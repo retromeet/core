067a4af1453290a1ca473da99c0bf44c07eeca6e
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-02-14 18:40:36 +0100

    Add js code to open the burger menu

--- app/api/rodauth_middleware.rb
+++ app/api/rodauth_middleware.rb
@@ -15,7 +15,7 @@ module API
     plugin :public, root: File.expand_path("../assets/images", __dir__)
     plugin :flash
     # plugin :middleware
-    plugin :assets, css: "layout.scss", path: File.expand_path("../assets", __dir__)
+    plugin :assets, css: "layout.scss", js: "base.js", path: File.expand_path("../assets", __dir__)
 
     plugin :render, views: File.expand_path("../assets/html", __dir__), engine: "haml", engine_opts: { "haml" => { escape_html: false } }, template_opts: { default_encoding: "UTF-8" }
     plugin :additional_view_directories, [File.expand_path("../mail/html", __dir__)]
