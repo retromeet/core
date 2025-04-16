9138ef1c54dea7a1ba2de4a1a0f7e7b796fc7491
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-04-16 16:02:23 +0200

    Add fr to locales

--- config/locales.rb
+++ config/locales.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 I18n.load_path += Dir["#{File.expand_path("locales", __dir__)}/*.yml"]
-I18n.available_locales = %i[en pt-BR]
+I18n.available_locales = %i[fr en pt-BR].sort!
 I18n.default_locale = :en
